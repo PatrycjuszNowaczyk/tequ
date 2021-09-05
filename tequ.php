db52ecb93674a14619144fb5946dcbfc8ae33924
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2021-09-05 15:25:16 +0200

    first commit

---
+++ tequ.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Tequ
+Description: Create simple tests on your wordpress pages
+Version: 1.0
+Author: Patrycjusz Nowaczyk
+License: GPLv2 or later
+Text Domain: tequ
+*/
+
+
+
+?>
