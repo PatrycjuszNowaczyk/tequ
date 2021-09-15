55902bd13606aec384076288c141605d416aa191
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2021-09-15 17:27:32 +0200

    add ob_start() to shortcode function to prevent collisions in wp page builders

--- assets/js/tequ.js
+++ assets/js/tequ.js
@@ -9,38 +9,63 @@
 //  se - sporty ekstremalne
 //  swo - sporty wodne
 //  sr - sporty rekreacyjne
-console.log(tequCategories[0].category);
 
-const testCategories = {
-    'sz': 0,
-    'si': 0,
-    'sl': 0,
-    'szi': 0,
-    'sw': 0,
-    'se': 0,
-    'swo': 0,
-    'sr': 0
-};
 
-const summaries = {
-    'sz': 'Wygląda na to, że lubisz być częścią zespołu i mieć w nim jakąś funkcję, ważny jest dla Ciebie wspólny cel, ale także rywalizacja. Spróbuj swoich sił w sportach zespołowych – do najpopularniejszych należą piłka nożna, koszykówka, siatkówka, czy piłka ręczna. Warto sprawdzić także takie dyscypliny, jak baseball (lub jego polską wersję, czyli palanta), rugby, hokej na lodzie i inne.',
-    'si': 'Być może nie jesteś typowym introwertykiem, ale lubisz spędzać czas ze sobą – również jeśli chodzi o aktywność fizyczną. A może po prostu lubisz być sam sobie „sterem, żaglem, okrętem i żeglarzem”? Spróbuj dyscypliny dla siebie! Może kolarstwo, badminton, tenis ziemny i stołowy, bieganie, lekkoatletyka, pływanie, siłownia i fitness albo joga?',
-    'sl': 'Jeśli już rozpocząć przygodę ze sportem, to tylko w okresie letnim! Przynajmniej ta pora będzie najbardziej odpowiednia dla Ciebie. Sprawdź, czy sporty typowo letnie będą dla Ciebie najlepsze – na przykład rolki, kolarstwo, frisbee, siatkówka plażowa, windsurfing, wakeboarding, tenis ziemny, nordic walking i inne.',
-    'szi': 'Czy zima to Twoja ulubiona pora roku? Chyba tak, bo wygląda na to, że najbardziej interesują Cię sporty zimowe. Proponujemy więc, aby swoją przygodę ze sportem rozpoczął/rozpoczęła od dyscyplin takich, jak: narciarstwo, snowboarding, hokej na lodzie, łyżwiarstwo, saneczkarstwo.',
-    'sw': 'Lubisz rywalizację i kontakt z drugim człowiekiem, masz też dużo energii, której nie masz gdzie spożytkować. Sporty walki, które możemy Ci zarekomendować to boks, karate, kickboxing, taekwondo, ju-jitsu, judo, zapasy. Sprawdź, które będą najlepsze dla Ciebie!',
-    'se': 'Lubisz, kiedy poziom adrenaliny w Twojej krwi odrobinę się podwyższa, zaś z ryzykiem i brawurą jesteś za pan brat! Myślałeś/myślałaś kiedyś o przygodzie ze sportami ekstremalnymi, takimi jak: bungee jumping, skoki spadochronowe, wspinaczka, kajakarstwo górskie, rower górski, rajdy samochodowe i inne.',
-    'swo': 'Lubisz kontakt z wodą – nie ważne czy w basenie, w jeziorze, czy na morzu! Sporty wodne to coś, co pewnie przypadnie Ci do gustu. Sprawdź na przykład pływanie, kajakarstwo, wioślarstwo, surfing, żeglarstwo, aqua-aerobic.',
-    'sr': 'Lubisz spokój, wyciszenie i relaks. Nie przepadasz za intensywnym wysiłkiem, ale lubisz ruch. Rekreacja to coś dla Ciebie – spróbuj jogi, nordic walking, gimnastyka.'
-};
 
-const testQuestions = document.querySelectorAll('.test__question');
-const testButtons = document.querySelectorAll('.test__answer-button');
+// return associative array of categories id's with 0 value
+function getTequCatIds(cat) {
+    let categories = {};
+    for (let i = 0; i < cat.length; i++) {
+        categories[cat[i].id] = 0;
+    };
+    return categories;
+}
+
+function getTequCatSummaries(cat) {
+    let description = {};
+    for (let i = 0; i < cat.length; i++) {
+        description[cat[i].id] = `${cat[i].description}`;
+    };
+    return description;
+}
+
+function getCategoriesData(aCategories){
+    let categories = {};
+    for(let i = 0; i < aCategories.length; i++){
+        categories[aCategories[i].id] = {};
+        for( let prop in aCategories[i]){
+            if(prop != 'id'){
+                categories[aCategories[i].id][prop] = aCategories[i][prop];
+            }
+        }
+    }
+    return categories;
+}
+getCategoriesData(tequCategories);
+// variable tequCategories is created by show-test.php file
+const tequObjCategories = getCategoriesData(tequCategories);
+const tequCategoriesCount = getTequCatIds(tequCategories);
+const summaries = getTequCatSummaries(tequCategories);
+
+// const summaries = {
+//     'sz': 'Wygląda na to, że lubisz być częścią zespołu i mieć w nim jakąś funkcję, ważny jest dla Ciebie wspólny cel, ale także rywalizacja. Spróbuj swoich sił w sportach zespołowych – do najpopularniejszych należą piłka nożna, koszykówka, siatkówka, czy piłka ręczna. Warto sprawdzić także takie dyscypliny, jak baseball (lub jego polską wersję, czyli palanta), rugby, hokej na lodzie i inne.',
+//     'si': 'Być może nie jesteś typowym introwertykiem, ale lubisz spędzać czas ze sobą – również jeśli chodzi o aktywność fizyczną. A może po prostu lubisz być sam sobie „sterem, żaglem, okrętem i żeglarzem”? Spróbuj dyscypliny dla siebie! Może kolarstwo, badminton, tenis ziemny i stołowy, bieganie, lekkoatletyka, pływanie, siłownia i fitness albo joga?',
+//     'sl': 'Jeśli już rozpocząć przygodę ze sportem, to tylko w okresie letnim! Przynajmniej ta pora będzie najbardziej odpowiednia dla Ciebie. Sprawdź, czy sporty typowo letnie będą dla Ciebie najlepsze – na przykład rolki, kolarstwo, frisbee, siatkówka plażowa, windsurfing, wakeboarding, tenis ziemny, nordic walking i inne.',
+//     'szi': 'Czy zima to Twoja ulubiona pora roku? Chyba tak, bo wygląda na to, że najbardziej interesują Cię sporty zimowe. Proponujemy więc, aby swoją przygodę ze sportem rozpoczął/rozpoczęła od dyscyplin takich, jak: narciarstwo, snowboarding, hokej na lodzie, łyżwiarstwo, saneczkarstwo.',
+//     'sw': 'Lubisz rywalizację i kontakt z drugim człowiekiem, masz też dużo energii, której nie masz gdzie spożytkować. Sporty walki, które możemy Ci zarekomendować to boks, karate, kickboxing, taekwondo, ju-jitsu, judo, zapasy. Sprawdź, które będą najlepsze dla Ciebie!',
+//     'se': 'Lubisz, kiedy poziom adrenaliny w Twojej krwi odrobinę się podwyższa, zaś z ryzykiem i brawurą jesteś za pan brat! Myślałeś/myślałaś kiedyś o przygodzie ze sportami ekstremalnymi, takimi jak: bungee jumping, skoki spadochronowe, wspinaczka, kajakarstwo górskie, rower górski, rajdy samochodowe i inne.',
+//     'swo': 'Lubisz kontakt z wodą – nie ważne czy w basenie, w jeziorze, czy na morzu! Sporty wodne to coś, co pewnie przypadnie Ci do gustu. Sprawdź na przykład pływanie, kajakarstwo, wioślarstwo, surfing, żeglarstwo, aqua-aerobic.',
+//     'sr': 'Lubisz spokój, wyciszenie i relaks. Nie przepadasz za intensywnym wysiłkiem, ale lubisz ruch. Rekreacja to coś dla Ciebie – spróbuj jogi, nordic walking, gimnastyka.'
+// };
+
+const tequQuestions = document.querySelectorAll('.test__question');
+const tequButtons = document.querySelectorAll('.test__answer-button');
 let result = new Array;
 let testIndex = 0;
 let testAnimTime = 300;
 let transition = `opacity ${testAnimTime}ms ease-in`;
 // ustawienie transition dla każdego pytania
-testQuestions.forEach(question => {
+tequQuestions.forEach(question => {
     question.style.transition = transition;
 });
 
@@ -66,9 +91,14 @@ function showResult(aArr) {
     i = 0;
     let paragraphs = new Array;
     biggestKey.forEach(key => {
-        paragraphs[i] = document.createElement('p');
+        paragraphs[i] = document.createElement('div');
         paragraphs[i].classList.add('test__score-paragraph');
-        paragraphs[i].innerHTML = summaries[key];
+        paragraphs[i].insertAdjacentHTML('beforeend', `<h3>${tequObjCategories[key].header}</h3>`)
+        paragraphs[i].insertAdjacentHTML('beforeend', `<img src="${tequObjCategories[key].image}">`);
+        // paragraphs[i].insertAdjacentHTML('beforeend', `<div class="tequ__score-img-wrapper>"<img src="${tequObjCategories[key].image}"></div>`);
+        paragraphs[i].insertAdjacentHTML('beforeend', `<p>${tequObjCategories[key].description}</p>`)
+        // paragraphs[i].innerHTML = summaries[key];
+        // paragraphs[i].append(`<img src="${tequObjCategories[key].image}" style="max-width: 100%" >`);
         document.querySelector('.test__score').appendChild(paragraphs[i]);
         paragraphs[i].style['transition'] = `opacity ${resultAnimTime}ms ease-in`;
         i < 4 ? resultAnimTime += testAnimTime : void 0;
@@ -98,30 +128,33 @@ document.querySelector('.test__question-enter').addEventListener('click', functi
     document.querySelector('.test__question--intro').style.opacity = '0';
     setTimeout(() => {
         document.querySelector('.test__question--intro').style.removeProperty('display');
-        testQuestions[testIndex].style.display = 'block';
-        testQuestions[testIndex].style.opacity = '1';
+        tequQuestions[testIndex].style.display = 'block';
+        tequQuestions[testIndex].style.opacity = '1';
     }, testAnimTime);
 });
 
-testButtons.forEach(button => {
+tequButtons.forEach(button => {
     button.addEventListener('click', function () {
         let categories = this.dataset.testcategory.split(",");
         categories.forEach(cat => {
-            testCategories[cat]++;
+            let split = cat.split('-');
+            let category = split[0];
+            let value = split[1] == 0 ? 1 : split[1];
+            tequCategoriesCount[category] += parseInt(value);
         });
-        if (testQuestions[testIndex + 1] == undefined) {
-            testQuestions[testIndex].style.opacity = '0';
+        if (tequQuestions[testIndex + 1] == undefined) {
+            tequQuestions[testIndex].style.opacity = '0';
             setTimeout(() => {
-                testQuestions[testIndex].style.removeProperty('display');
-                showResult(testCategories);
+                tequQuestions[testIndex].style.removeProperty('display');
+                showResult(tequCategoriesCount);
             }, testAnimTime);
         }
         else {
-            testQuestions[testIndex].style.opacity = '0';
+            tequQuestions[testIndex].style.opacity = '0';
             setTimeout(() => {
-                testQuestions[testIndex].style.removeProperty('display');
-                testQuestions[++testIndex].style.display = 'block';
-                testQuestions[testIndex].style.opacity = '1';
+                tequQuestions[testIndex].style.removeProperty('display');
+                tequQuestions[++testIndex].style.display = 'block';
+                tequQuestions[testIndex].style.opacity = '1';
             }, testAnimTime);
         }
     });
--- includes/show-test.php
+++ includes/show-test.php
@@ -1,55 +1,88 @@
 <?php
 
 function tequ_show_test( $atts ){
-    // get data from database
-    $post_meta = get_post_meta($atts['id'], 'tequ_data', true);
-
-    // enqueue required scripts and styles
-    wp_enqueue_style('tequ-style', TEQU_PLUGIN_URL . 'assets/css/tequ-style.css', null, '1.0', false);
-    wp_enqueue_script('tequ-script', TEQU_PLUGIN_URL . 'assets/js/tequ.js', null, '1.0', true);
-
-    function getCategories($answer){
-        $aCategories = explode(':', $answer);
-        return $aCategories[1];
-    };
-    function getAnswer($answer){
-        $aCategories = explode(':', $answer);
-        return $aCategories[0];
-    };
-
-?>
-<script>
-    <?php // set array of categories objects ?>
-    const tequCategories = [<?php $arrLength = count($post_meta['category']);
-            for ($i = 0; $i < $arrLength; $i++){
-                echo '{';
-                echo '\'category\' :' . (isset($post_meta['category'][$i]) ? '\'' . $post_meta['category'][$i] . '\',' : '\'\',');
-                echo '\'id\' :' . (isset($post_meta['category-id'][$i]) ? '\'' . $post_meta['category-id'][$i] . '\',' : '\'\',');
-                echo '\'header\' :' . (isset($post_meta['category-header'][$i]) ? '\'' . $post_meta['category-header'][$i] . '\',' : '\'\',');
-                echo '\'description\' :' . (isset($post_meta['category-description'][$i]) ? '\'' . $post_meta['category-description'][$i] . '\',' : '\'\',');
-                echo '\'image\' :' . (isset($post_meta['category-image'][$i]) ? '\'' . $post_meta['category-image'][$i] . '\'' : '\'\'');
-                echo '}';
-                if($i < $arrLength - 1 ){
-                    echo ', ';
-            }
-        } ?>];
-</script>
-<div class="test">
-        <div class="test__header">
-            <h2>
-                <?php echo isset($post_meta['header']) ? $post_meta['header'] : ''; ?>
-            </h2>
+    ob_start();
+    if (false){ ?>
+        <div>
+            <p>TEST SHORTCODE</p>
+            <p>Here you will see test</p>
         </div>
-        <div class="test__questions">
-            <div class="test__question--intro">
-                <h3>
-                <?php echo isset($post_meta['description']) ? $post_meta['description'] : ''; ?>
-                </h3>
-                <button class="test__question-enter"><?php echo isset($post_meta['begin-button']) ? $post_meta['begin-button'] : ''; ?></button>
+    <?php   
+        $content = ob_get_clean();
+        return $content;
+    } else{
+        ob_start(); ?>
+
+        <?php if(false){ ?>
+            <h1>bla bla bla</h1>
+        <?php } ?>
+
+        <?php    // get data from database
+        $post_meta = get_post_meta($atts['id'], 'tequ_data', true);
+
+        // enqueue required scripts and styles
+        if(\Elementor\Plugin::$instance->editor->is_edit_mode()){
+            add_action('elementor/editor/after_register_scripts', function(){
+                wp_register_script('tequ-script', TEQU_PLUGIN_URL . 'assets/js/tequ.js', '', '1.0', true);
+            });
+            add_action('elementor/editor/after_enqueue_scripts', function(){
+                wp_enqueue_script('tequ-script');
+                // wp_enqueue_script('tequ-script');
+            });
+            add_action('elementor/editor/after_enqueue_styles', function(){
+                wp_enqueue_style('tequ-style', TEQU_PLUGIN_URL . 'assets/css/tequ-style.css', null, '1.0', false);
+            });
+            // wp_enqueue_script('tequ-script', TEQU_PLUGIN_URL . 'assets/js/tequ.js', null, '1.0', true);
+            // wp_enqueue_style('tequ-style', TEQU_PLUGIN_URL . 'assets/css/tequ-style.css', null, '1.0', false);
+
+        }
+        else{
+            wp_enqueue_style('tequ-style', TEQU_PLUGIN_URL . 'assets/css/tequ-style.css', null, '1.0', false);
+            wp_enqueue_script('tequ-script', TEQU_PLUGIN_URL . 'assets/js/tequ.js', null, '1.0', true);
+        }
+
+        function tequ_getCategories($answer){
+            $aCategories = explode(':', $answer);
+            return $aCategories[1];
+        };
+        function tequ_getAnswer($answer){
+            $aCategories = explode(':', $answer);
+            return $aCategories[0];
+        };
+
+        ?>
+        <script>
+            <?php // set array of categories objects ?>
+            const tequCategories = [<?php $arrLength = count($post_meta['category']);
+                    for ($i = 0; $i < $arrLength; $i++){
+                        echo '{';
+                        echo '\'category\' :' . (isset($post_meta['category'][$i]) ? '\'' . $post_meta['category'][$i] . '\',' : '\'\',');
+                        echo '\'id\' :' . (isset($post_meta['category-id'][$i]) ? '\'' . $post_meta['category-id'][$i] . '\',' : '\'\',');
+                        echo '\'header\' :' . (isset($post_meta['category-header'][$i]) ? '\'' . $post_meta['category-header'][$i] . '\',' : '\'\',');
+                        echo '\'description\' :' . (isset($post_meta['category-description'][$i]) ? '\'' . $post_meta['category-description'][$i] . '\',' : '\'\',');
+                        echo '\'image\' :' . (isset($post_meta['category-image'][$i]) ? '\'' . $post_meta['category-image'][$i] . '\'' : '\'\'');
+                        echo '}';
+                        if($i < $arrLength - 1 ){
+                            echo ', ';
+                    }
+                } ?>];
+        </script>
+        <div class="test">
+            <div class="test__header">
+                <h2>
+                    <?php echo isset($post_meta['header']) ? $post_meta['header'] : ''; ?>
+                </h2>
             </div>
-            <?php // start questions loop ?>
-            <?php $arrLength = count($post_meta['question']);
-            for ($i = 0; $i < $arrLength; $i++){ ?>
+            <div class="test__questions">
+                <div class="test__question--intro">
+                    <h3>
+                    <?php echo isset($post_meta['description']) ? $post_meta['description'] : ''; ?>
+                    </h3>
+                    <button class="test__question-enter"><?php echo isset($post_meta['begin-button']) ? $post_meta['begin-button'] : ''; ?></button>
+                </div>
+                <?php // start questions loop ?>
+                <?php $arrLength = count($post_meta['question']);
+                for ($i = 0; $i < $arrLength; $i++){ ?>
 
                 <div class="test__question">
                     <div class="test__question-paragraph">
@@ -58,15 +91,18 @@ function tequ_show_test( $atts ){
                         </p>
                     </div>
                     <div class="test__question-answer">
-                        <button class="test__answer-button" data-testCategory="<?php echo getCategories($post_meta['answer-1'][$i]); ?>"> <?php echo isset($post_meta['answer-1'][$i]) ? getAnswer($post_meta['answer-1'][$i]) : ''; ?> </button>
-                        <button class="test__answer-button" data-testCategory="<?php echo getCategories($post_meta['answer-2'][$i]); ?>"> <?php echo isset($post_meta['answer-2'][$i]) ? getAnswer($post_meta['answer-2'][$i]) : ''; ?> </button>
+                        <button class="test__answer-button" data-testCategory="<?php echo tequ_getCategories($post_meta['answer-1'][$i]); ?>"> <?php echo isset($post_meta['answer-1'][$i]) ? tequ_getAnswer($post_meta['answer-1'][$i]) : ''; ?> </button>
+                        <button class="test__answer-button" data-testCategory="<?php echo tequ_getCategories($post_meta['answer-2'][$i]); ?>"> <?php echo isset($post_meta['answer-2'][$i]) ? tequ_getAnswer($post_meta['answer-2'][$i]) : ''; ?> </button>
                     </div>
                 </div>
 
-            <?php // end questions loop ?>
-            <?php } ?>
-            <div class="test__score">
+                <?php // end questions loop ?>
+                <?php } ?>
+                <div class="test__score"></div>
             </div>
         </div>
-    </div>
-<?php }; ?>
+    <?php 
+        $content = ob_get_clean();
+        return $content;
+    };
+} ?>
